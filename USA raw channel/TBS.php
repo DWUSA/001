9b283e1047808959c9631c4eb33516df3f998923
DMRWOLFIE <55549841+DMRWOLFIE@users.noreply.github.com>
2022-02-19 12:27:45 -0400

    Update TBS.php

--- USA raw channel/TBS.php
+++ USA raw channel/TBS.php
@@ -9,10 +9,12 @@ $str2hash = $key . $today .
 $validminutes;
 $md5raw = md5($str2hash, true);
 $base64hash = base64_encode($md5raw);
-$urlsignature = "server_time="
-$today . "&hash_value=" . $base64hash.
+$urlsignature = "server_time=".
+$today ."&hash_value=" . $base64hash.
  "&validminutes=$validminutes";
-&base64url($urlsignature);
+base64url($urlsignature);
+
+?>
 
 <?php echo "{$base_url}?
 wmsAuthSign=$base64urlsignature";?>
